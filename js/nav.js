0a45cb72a984350c4250d322df356897b15e2fb3
Raloseq <45914275+Raloseq@users.noreply.github.com>
2019-01-31 20:01:04 +0100

    Add menu with animations

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,10 @@
-const openMobileNav = document.querySelector('.app-nav--burger');
-openMobileNav.addEventListener('click', function () {
-    openMobileNav.classList.toggle('app-nav--burger__active');
-})
+const burgerAnimation = document.querySelector('.app-nav--burger');
+const openMobileNav = document.querySelector('.app-nav');
+const navItems = document.querySelectorAll('.app-nav--item');
+burgerAnimation.addEventListener('click', function () {
+    burgerAnimation.classList.toggle('app-nav--burger__active');
+    openMobileNav.classList.toggle('active');
+    navItems.forEach(function (e) {
+        e.classList.toggle('active-item');
+    })
+});
