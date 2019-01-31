9417f64fdd1937d34d9aa3f93023c8db66bcd2f2
Raloseq <45914275+Raloseq@users.noreply.github.com>
2019-01-31 14:34:29 +0100

    Adding some files

---
+++ js/nav.js
@@ -0,0 +1,4 @@
+const openMobileNav = document.querySelector('.app-nav--burger');
+openMobileNav.addEventListener('click', function () {
+    openMobileNav.classList.toggle('app-nav--burger__active');
+})
