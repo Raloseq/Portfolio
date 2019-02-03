385506dbefbbb64330cec4e362d8c6e6793d429b
Raloseq <45914275+Raloseq@users.noreply.github.com>
2019-02-03 23:06:05 +0100

    Js animation about

--- js/about.js
+++ js/about.js
@@ -2,9 +2,16 @@ const aboutH1 = document.querySelector('.app-about--h1');
 const aboutP = document.querySelector('.app-about--p');
 
 window.addEventListener('scroll', function () {
-    const lastPosition = window.scrollY;
-    console.log(lastPosition);
-    if (lastPosition > 150) {
+    let isInViewport = function (elem) {
+        let bounding = elem.getBoundingClientRect();
+        return (
+            bounding.top >= 0 &&
+            bounding.left >= 0 &&
+            bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    };
+    if (isInViewport(aboutH1) && isInViewport(aboutP)) {
         aboutH1.classList.add('app-about--h1__active');
         aboutP.classList.add('app-about--p__active');
     } else {
