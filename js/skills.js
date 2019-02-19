38a8a7cc7e5de42dedffa9b00f0d4c3fba23738e
Raloseq <45914275+Raloseq@users.noreply.github.com>
2019-02-19 16:02:05 +0100

    skills animation

--- js/skills.js
+++ js/skills.js
@@ -2,10 +2,14 @@ import {
     isInViewport
 } from './isInViewport.js';
 const skillsDiv = document.querySelectorAll('.app-skills--div');
+const skillsDivH1 = document.querySelector('.app-skills--h1')
 window.addEventListener('scroll', () => {
-    if (isInViewport(skillsDiv)) {
+    if (isInViewport(skillsDivH1)) {
+        skillsDivH1.classList.add('app-skills--h1__active');
+    }
+    if (isInViewport(...skillsDiv)) {
         skillsDiv.forEach((e) => {
-            e.style.backgroundColor = "red";
+            e.classList.add('skills-div__active');
         })
     }
 });
---
+++ js/typing.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    backSpeed: 50,
+    typeSpeed: 50,
+    loop: true
+});
